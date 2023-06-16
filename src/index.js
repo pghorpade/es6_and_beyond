e3f304f14556ec8589ab799e01c2f4d925123406
Parinita Mulak <pghorpade@library.ucla.edu>
2023-06-16 10:43:37 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import "./styles.css";
+
+// Difference between let and const
+
+// const name = "Parinita"
+// name = "Mulak"
+
+let name = "Parinita";
+name = "Mulak";
+
+console.log(name);
+
+// what is difference between var and [let and const]
+
+// lets write a for loop with a var
+
+for (var i = 0; i < 5; i++) {
+  console.log(i);
+}
+console.log(i);
